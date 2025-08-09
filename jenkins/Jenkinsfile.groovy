bf024aeeb1a4abb4a1cc4ef91205c72fe4bb96d5
Onkar Salunke <onkarsalunke9@gmail.com>
2025-08-09 23:59:33 +0530

    added extention to jenkinsfile

---
+++ jenkins/Jenkinsfile.groovy
@@ -0,0 +1,31 @@
+pipeline {
+    agent any
+    environment {
+        ARM_SUBSCRIPTION_ID = credentials('azure-subscription-id')
+        ARM_CLIENT_ID = credentials('azure-client-id')
+        ARM_CLIENT_SECRET = credentials('azure-client-secret')
+        ARM_TENANT_ID = credentials('azure-tenant-id')
+    }
+    stages {
+        stage('Terraform Init'){
+            steps {
+                sh 'terraform init'
+            }
+        }
+        stage('Terraform Plan'){
+            steps {
+                sh 'terraform plan'
+            }
+        }
+        stage('Terraform Apply'){
+            steps {
+                sh 'terraform apply -auto-approve'
+            }
+        }
+    }
+    post {
+        failure {
+            echo 'Terraform failed!'
+        }
+    }
+}
